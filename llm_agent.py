eb17358f86c0b3d8ef1a712555ef22b32641296e
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2024-09-01 18:06:15 +0200

    remove unused code

--- llm_agent.py
+++ llm_agent.py
@@ -11,8 +11,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-PROFIT_MULTIPLIER = int(os.getenv('PROFIT_MULTIPLIER'))
-
 
 class LlmAgent:
     def __init__(self):
